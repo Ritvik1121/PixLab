c7949bc20a768f573b0cf58d6e49f1512d705821
Ritvik <rintu1121@outlook.com>
2018-12-18 07:51:49 -0700

    Finish Pixlab

--- src/PictureTester.java
+++ src/PictureTester.java
@@ -137,6 +137,13 @@ public class PictureTester
     swan.explore();
   }
 
+  public static void testEdgeDetection2()
+  {
+    Picture swan = new Picture("swan.jpg");
+    swan.edgeDetection2(10);
+    swan.explore();
+  }
+
   /** Main method for testing.  Every class can have a main
     * method in Java */
   public static void main(String[] args)
@@ -161,10 +168,10 @@ public class PictureTester
     //testMirrorGull();
     //testMirrorDiagonal();
     //testCollage();
-    testMyCollage();
+    //testMyCollage();
     //testCopy();
     //testEdgeDetection();
-    //testEdgeDetection2();
+    testEdgeDetection2();
     //testChromakey();
     //testEncodeAndDecode();
     //testGetCountRedOverValue(250);
